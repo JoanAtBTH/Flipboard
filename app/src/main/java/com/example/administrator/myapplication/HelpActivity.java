acecf7f7e0a0945d3d33c1e686c966cdda918c90
joancolom <joan8989@gmail.com>
2016-12-01 17:54:23 +0100

    Help information showed Help text random, to be defined.

--- app/src/main/java/com/example/administrator/myapplication/HelpActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/HelpActivity.java
@@ -13,12 +13,21 @@ import android.content.Intent;
 
 public class HelpActivity extends AppCompatActivity {
     private TextView textView5;
+    private final String helpInfo = "What is Lorem Ipsum?\n" +
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem " +
+            "Ipsum has been the industry's standard dummy text ever since the 1500s, when an "+
+            "unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+            "It has survived not only five centuries, but also the leap into electronic " +
+            "typesetting, remaining essentially unchanged. It was popularised in the 1960s with " +
+            "the release of Letraset sheets containing Lorem Ipsum passages, and more recently " +
+            "with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         textView5 = (TextView) findViewById(R.id.textView5);
+        textView5.setText(getHelpInfo());
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,5 +50,13 @@ public class HelpActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+     * Getters
+     */
+
+    public String getHelpInfo() {
+        return helpInfo;
     }
 }
